59327619513f847c896b7816b67edf8e9a06f505
Brandon Phillips <brandon.phillips976@gmail.com>
2020-11-21 22:14:24 -0800

    added contact form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import './index.css'
 
 export default function ContactForm() {
     return (
-        <div>
-            
-        </div>
+        <form>
+            <h1 className='text-center'>Contact Me</h1>
+            <div className="form-group">
+                <label for="NameInput">Name</label>
+                <input type="email" className="form-control" id="NameInput" placeholder="name@example.com" />
+            </div>
+            <div className="form-group">
+                <label for="emailInput">Email address</label>
+                <input type="email" className="form-control" id="emailInput" placeholder="name@example.com" />
+            </div>
+            <div class="form-group">
+                <label for="messageInput">Message</label>
+                <textarea class="form-control" id="messageInput" rows="3"></textarea>
+            </div>
+        </form>
     )
 }
